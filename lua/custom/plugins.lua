8e613d14b556497c31200619dad914e0c6f8af2b
ragilalfarizi <alfariziragill@gmail.com>
2023-09-30 01:45:41 +0700

    feat: add illuminate plugin

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -88,7 +88,7 @@ local plugins = {
   },
 
   -- hlargs
-   {
+  {
     "m-demare/hlargs.nvim",
     event = "BufWinEnter",
     config = function()
@@ -98,6 +98,40 @@ local plugins = {
     end,
   },
 
+  -- illuminate
+  {
+    "RRethy/vim-illuminate",
+    event = { "CursorHold", "CursorHoldI" },
+    dependencies = "nvim-treesitter",
+    config = function()
+      require("illuminate").configure {
+        under_cursor = true,
+        max_file_lines = nil,
+        delay = 100,
+        providers = {
+          "lsp",
+          "treesitter",
+          "regex",
+        },
+        filetypes_denylist = {
+          "NvimTree",
+          "Trouble",
+          "Outline",
+          "TelescopePrompt",
+          "Empty",
+          "dirvish",
+          "fugitive",
+          "alpha",
+          "packer",
+          "neogitstatus",
+          "spectre_panel",
+          "toggleterm",
+          "DressingSelect",
+          "aerial",
+        },
+      }
+    end,
+  },
 }
 
 return plugins
